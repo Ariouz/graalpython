361e457075b6cf113c47859715f7d140dd078dac
Virgil Calvez <virgil.calvez@oracle.com>
2025-12-04 15:46:55 +0100

    -J-XX...........

--- mx.graalpython/mx_graalpython.py
+++ mx.graalpython/mx_graalpython.py
@@ -266,7 +266,7 @@ def github_ci_build_args():
             # f"-J-Xms{build_mem}m",
             f"-J-Xms7g",
             f"--parallelism={parallelism}",
-            "-XX:GCTimeRatio=3"
+            "-J-XX:GCTimeRatio=3"
         ]
 
 def libpythonvm_build_args():
